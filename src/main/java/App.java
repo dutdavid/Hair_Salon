7b5e9d215205e80b6fa3649d85b7fad6f733d11a
root <root@localhost.localdomain>
2019-02-12 08:54:37 +0300

    new changes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,15 +7,19 @@ import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
-    // ProcessBuilder process = new ProcessBuilder();
-    //      Integer port;
-    //      if (process.environment().get("PORT") != null) {
-    //          port = Integer.parseInt(process.environment().get("PORT"));
-    //      } else {
-    //          port = 4567;
-    //      }
-    //
-    //     setPort(port);
+    ProcessBuilder process = new ProcessBuilder(); 
+    Integer port;
+
+    // This tells our app that if Heroku sets a port for us, we need to use that port.
+    // Otherwise, if they do not, continue using port 4567.
+
+    if (process.environment().get("PORT") != null) {
+        port = Integer.parseInt(process.environment().get("PORT"));
+    } else {
+        port = 4567;
+    }
+
+    setPort(port);
 
         staticFileLocation("/public");
             String layout = "templates/layout.vtl";
